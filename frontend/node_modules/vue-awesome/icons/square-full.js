cb20f7ca475491f8d599dcf410381ad7e2f24815
Maria <marysia.harcej@gmail.com>
2018-07-02 14:18:14 +0200

    Revert "added frontend data"
    This reverts commit 986667d7b55b39e9c3b1dd710181b24456eb11e5.

--- frontend/node_modules/vue-awesome/icons/square-full.js
+++
@@ -1,3 +0,0 @@
-import Icon from '../components/Icon.vue'
-
-Icon.register({"square-full":{"width":512,"height":512,"paths":[{"d":"M512 512H0V0H512V512z"}]}})
--- frontend/node_modules/vue-awesome/icons/volume-off.js
+++
@@ -1,3 +0,0 @@
-import Icon from '../components/Icon.vue'
-
-Icon.register({"volume-off":{"width":256,"height":512,"paths":[{"d":"M256 88V424C256 445.4 230.1 456 215 441L126.1 352H24C10.7 352 0 341.3 0 328V184C0 170.7 10.7 160 24 160H126.1L215 71C230 56 256 66.6 256 88z"}]}})
