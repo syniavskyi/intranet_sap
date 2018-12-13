29ba5a81b68f491e787bcd670859f90cfb44d40e
Pavlo <syniavskyi.pavlo@gmail.com>
2018-12-13 16:10:21 +0100

    Added support for Ukrainian language

--- src/lang/lang.js
+++ src/lang/lang.js
@@ -4,20 +4,22 @@ import VueI18n from 'vue-i18n'
 import en from './i18n/en.json'
 import pl from './i18n/pl.json'
 import de from './i18n/de.json'
+import ua from './i18n/ua.json'
 
 Vue.use(VueI18n)
 
 var userLang = localStorage.getItem('lang') || navigator.language || navigator.userLanguage; 
 var locale = userLang.slice(0, 2).toLowerCase();
 
-if (locale !== 'pl' && locale !== 'en' && locale !== 'de') {
+if (locale !== 'pl' && locale !== 'en' && locale !== 'de' && locale !== 'ua') {
     locale = 'en';
 }
 
 const messages = {
     pl: pl,
     en: en,
-    de: de
+    de: de,
+    ua: ua
 }
 
 const i18n = new VueI18n({
