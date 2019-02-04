ff0075d0a1b08a39b2f96c3564df0212df119d18
Kacper Borkowski <k.borkowski0@gmail.com>
2019-02-04 11:43:38 +0100

    Added filtering based on department or roles

--- src/components/content/Calendar.vue
+++ src/components/content/Calendar.vue
@@ -6,8 +6,8 @@
       <div class="component-content">
         <div class="content-header">
           <div class="content-header-title-and-menu">
-          <!-- <img src="../../assets/images/nav/if_menu-32.png" width="32px" class="content-header-menu"> -->
-          <div @click="showMenu" class="content-header-menu">&#9776;</div>
+            <!-- <img src="../../assets/images/nav/if_menu-32.png" width="32px" class="content-header-menu"> -->
+            <div @click="showMenu" class="content-header-menu">&#9776;</div>
             <p class="content-header-title">{{ $t("header.calendar") }}</p>
           </div>
         </div>
@@ -15,12 +15,14 @@
           <div class="calendar-in-row">
             <div class="cal-and-fils">
               <div>
-                <v-date-picker mode='single' :min-date="new Date()" v-model="addEvent.DateFrom" :attributes="attributes" is-inline @dayclick='dayClicked'/>
+                <v-date-picker mode="single" :min-date="new Date()" v-model="addEvent.DateFrom" :attributes="attributes" is-inline @dayclick="dayClicked" />
               </div>
               <div class="cal-filters">
                 <div class="cd-for-select">
-                  <select required class="cd-select" v-model="filters.department">
-                    <option v-for="department in departmentList" :key="department.Key" :value="department.Key">{{ department.Value }}</option>
+                  <select required class="cd-select" v-model="filters.department" @change="filterUser($event)">
+                    <option v-for="department in departmentList" :key="department.Key" :value="department.Key">
+                      {{ department.Value }}
+                    </option>
                   </select>
                   <label class="cd-slabel">{{ $t("label.department") }}</label>
                 </div>
@@ -35,30 +37,34 @@
                 <button class="func-btn cal-btn" @click="clearFilters">{{ $t("button.clear") }}</button>
               </div>
             </div>
-            <div v-if='selectedDay' class="calendar-tile">
+            <div v-if="selectedDay" class="calendar-tile">
               <div class="add-event-header">
                 <h3 class="add-event-title">{{ selectedDay.date.toLocaleDateString() }}</h3>
                 <button @click="openDialog" class="button add-button">{{ $t("button.add") }}</button>
               </div>
               <div class="add-event-content">
                 <ul class="selected-day">
-                  <li v-for='attr in filterEventForDay' :key='attr.EventId' class="li-event">
-                    <div class="low-prio-event" v-if="attr.Priority=='L'"> </div>
-                    <div class="medium-prio-event" v-if="attr.Priority=='M'"> </div>
-                    <div class="high-prio-event" v-if="attr.Priority=='H'"> </div>
+                  <li v-for="attr in filterEventForDay" :key="attr.EventId" class="li-event">
+                    <div class="low-prio-event" v-if="attr.Priority=='L'"></div>
+                    <div class="medium-prio-event" v-if="attr.Priority=='M'"></div>
+                    <div class="high-prio-event" v-if="attr.Priority=='H'"></div>
                     <div class="event-attr">
                       <div class="event-time">
-                        <div class="evt-desc"> {{ $t("label.shortTime")}} {{ formatTime(attr.EventTime)}} </div>
-                        <div class="evt-desc"> {{ formatDate(attr) }} </div>
+                        <div class="evt-desc">{{ $t("label.shortTime")}} {{ formatTime(attr.EventTime)}}</div>
+                        <div class="evt-desc">{{ formatDate(attr) }}</div>
                       </div>
                       <div class="event-attr-header">{{ attr.EventName }}</div>
-                      <div class="evt-desc"> {{ attr.Description}} </div>
-                      <div class="evt-type"> {{ attr.EventTypeName }} </div>
-                      <div class="evt-priv"> {{ attr.EventPrivacy }} </div>
+                      <div class="evt-desc">{{ attr.Description}}</div>
+                      <div class="evt-type">{{ attr.EventTypeName }}</div>
+                      <div class="evt-priv">{{ attr.EventPrivacy }}</div>
                     </div>
                     <div class="events-buttons">
-                      <button class="cal-btn-edit" :disabled="attr.CreatedBy !== loginAlias && authType !== '*'" @click="editEventClick(attr, $t)">{{ $t("button.edit") }}</button>
-                      <button class="cal-btn-del" :disabled="attr.CreatedBy !== loginAlias && authType !== '*'" @click="deleteEvent(attr, $t)">{{ $t("button.delete") }}</button>
+                      <button class="cal-btn-edit" :disabled="attr.CreatedBy !== loginAlias && authType !== '*'" @click="editEventClick(attr, $t)">
+                        {{ $t("button.edit") }}
+                      </button>
+                      <button class="cal-btn-del" :disabled="attr.CreatedBy !== loginAlias && authType !== '*'" @click="deleteEvent(attr, $t)">
+                        {{ $t("button.delete") }}
+                      </button>
                     </div>
                   </li>
                 </ul>
@@ -79,7 +85,7 @@ import i18n from "../../lang/lang";
 import Menu from "../Menu.vue";
 import Form from "../dialogs/EventFormDialog";
 import { mapGetters, mapActions } from "vuex";
-import Modal from '../dialogs/MessageLogDialog';
+import Modal from "../dialogs/MessageLogDialog";
 const utils = require("../../utils");
 
 export default {
@@ -95,9 +101,9 @@ export default {
   },
   created() {
     let oStore = this.$store;
-      oStore.commit('SET_PROMISE_TO_READ', oStore.getters.getEventsToRead);
-      oStore.dispatch('getData', null);
-      utils.checkAuthLink(this.$router, oStore.getters.getUserAuth.ZMENU);
+    oStore.commit("SET_PROMISE_TO_READ", oStore.getters.getEventsToRead);
+    oStore.dispatch("getData", null);
+    utils.checkAuthLink(this.$router, oStore.getters.getUserAuth.ZMENU);
   },
   computed: Object.assign(
     mapGetters({
@@ -111,131 +117,153 @@ export default {
       userData: "getUserInfo",
       authType: "getCalendarAuth",
       dialogEvent: "getDialogEvent"
-    }), {
+    }),
+    {
       filteredEvents() {
-      let aEvents = this.events,
-        aFilters = this.filters;
-      let aFilteredEvents = [];
-      if (
-        aFilters.department === null &&
-        aFilters.employee === null
-      ) {
+        let aEvents = this.events,
+          aFilters = this.filters;
+        let aFilteredEvents = [];
+        if (aFilters.department === null && aFilters.employee === null) {
+          return aEvents;
+        } else {
+          let fnFilter;
+          if (aFilters.department && aFilters.employee) {
+            fnFilter = function(oItem) {
+              return (
+                oItem.TargetGroup.includes(aFilters.department) &&
+                oItem.Employee.includes(aFilters.employee)
+              );
+            };
+          } else if (aFilters.department) {
+            fnFilter = function(oItem) {
+              return oItem.TargetGroup.includes(aFilters.department);
+            };
+          } else if (aFilters.employee) {
+            fnFilter = function(oItem) {
+              return oItem.Employee.includes(aFilters.employee);
+            };
+          }
+          if (fnFilter) {
+            aEvents = aEvents.filter(fnFilter);
+          }
+        }
         return aEvents;
-      } else {
+      },
+      // events filter by clicked day
+      filterEventForDay() {
+        let aEvents = this.events;
         let fnFilter;
-         if (aFilters.department && aFilters.employee) {
-          fnFilter = function(oItem) {
-            return (
-              oItem.TargetGroup.includes(aFilters.department) &&
-              oItem.Employee.includes(aFilters.employee)
-            );
-          };
-        }  else if (aFilters.department) {
-          fnFilter = function(oItem) {
-            return oItem.TargetGroup.includes(aFilters.department);
-          };
-        } else if (aFilters.employee) {
-          fnFilter = function(oItem) {
-            return oItem.Employee.includes(aFilters.employee);
-          };
-        }
-        if (fnFilter) {
-          aEvents = aEvents.filter(fnFilter);
-        }
-      }
-      return aEvents;
-    },
-    // events filter by clicked day
-    filterEventForDay() {
-      let aEvents = this.events;
-      let fnFilter;
-      let day = this.selectedDay.date;
+        let day = this.selectedDay.date;
 
         fnFilter = function(oItem) {
-            return oItem.DateFrom <= day.setHours(1) && day.setHours(1) <= oItem.DateTo;
-          };
-      return aEvents = aEvents.filter(fnFilter)
-    },
-    // calendar attributes
-    attributes() {
-      return this.filteredEvents.map(t => ({
-        dot: {
-          backgroundColor: t.color
-        },
-        highlight: {
-          backgroundColor: t.color
-        },
-        dates: {
-          start: t.DateFrom,
-          end: t.DateTo
-        },
-        customData: t,
-        popover: {
-          label: t.EventName
-        }
-      }));
-    }
+          return (
+            oItem.DateFrom <= day.setHours(1) && day.setHours(1) <= oItem.DateTo
+          );
+        };
+        return (aEvents = aEvents.filter(fnFilter));
+      },
+      // calendar attributes
+      attributes() {
+        return this.filteredEvents.map(t => ({
+          dot: {
+            backgroundColor: t.color
+          },
+          highlight: {
+            backgroundColor: t.color
+          },
+          dates: {
+            start: t.DateFrom,
+            end: t.DateTo
+          },
+          customData: t,
+          popover: {
+            label: t.EventName
+          }
+        }));
+      }
     }
   ),
   components: {
     "app-menu": Menu,
-    "modal": Modal,
+    modal: Modal,
     "event-form": Form
   },
   methods: Object.assign(
-    mapActions([
-      "clearForm",
-      "clearFilters",
-      "editEvent",
-      "performDialog"
-    ]), {
+    mapActions(["clearForm", "clearFilters", "editEvent", "performDialog"]),
+    {
       showMenu(event) {
-      let obj = { window, event };
-      this.$store.dispatch("setSideMenu", obj);
-    },
-    // day cklicked on v-calendar
-    dayClicked(day) {
-      // if selected date is later than today, don't show events
-      if (!utils.dateToValid(day.date, new Date(), "later")) return
-      this.selectedDay = day;
-    },
-    // modal for new event
-    openDialog() {
-      this.$store.commit("SET_DATE_FROM", this.addEvent.DateFrom);
-      this.clearForm();
-      this.performDialog();
-      this.displaySaveButton = true;
-    },
-    //open modal and assign data in edited event
-    editEventClick(data) {
-      this.performDialog();
-      let editedData = data;
-      Object.assign(this.addEvent, editedData);
-      this.displaySaveButton = false;
-    },
-    deleteEvent(data) {
-      let editedData = data;
-      editedData.Action = 'D'
-      Object.assign(this.addEvent, editedData);
-      this.editEvent(editedData);
-    },
-    formatDate(event) {
-      if (event.DateTo <= event.DateFrom) {
-        return moment(event.DateFrom).format("DD.MM");
-      } else {
-        return (
-          moment(event.DateFrom).format("DD.MM") +
-          " - " +
-          moment(event.DateTo).format("DD.MM")
-        );
-      }
-    },
-    formatTime(time) {
-      if(time) {
-         return time.slice(0, 5);
+        let obj = { window, event };
+        this.$store.dispatch("setSideMenu", obj);
+      },
+      filterUser(event) {
+        this.$store.commit('SET_DISPLAY_LOADER', true);
+
+        if(event.target.value !== "BTECH") {
+          const filterDep = (res) => res.DepartmentId === event.target.value;
+          const filterRole = (res) => res.Role === event.target.value;
+
+          this.$store.dispatch('getUsersLists').then(res => {
+            let usersFiltred = [];
+
+            usersFiltred = res.data.d.results.filter(user => filterDep(user));
+
+            if(usersFiltred.length === 0) {
+              usersFiltred = res.data.d.results.filter(user => filterRole(user));
+            }
+
+            this.$store.commit('GET_USER_LIST', usersFiltred);
+            this.$store.commit('SET_DISPLAY_LOADER', false);
+          });
+        } else {
+          this.$store.dispatch('getUsersLists').then(res => {
+            this.$store.commit('GET_USER_LIST', res.data.d.results);
+            this.$store.commit('SET_DISPLAY_LOADER', false);
+          });
+        }
+      },
+      // day clicked on v-calendar
+      dayClicked(day) {
+        // if selected date is later than today, don't show events
+        if (!utils.dateToValid(day.date, new Date(), "later")) return;
+        this.selectedDay = day;
+      },
+      // modal for new event
+      openDialog() {
+        this.$store.commit("SET_DATE_FROM", this.addEvent.DateFrom);
+        this.clearForm();
+        this.performDialog();
+        this.displaySaveButton = true;
+      },
+      //open modal and assign data in edited event
+      editEventClick(data) {
+        this.performDialog();
+        let editedData = data;
+        Object.assign(this.addEvent, editedData);
+        this.displaySaveButton = false;
+      },
+      deleteEvent(data) {
+        let editedData = data;
+        editedData.Action = "D";
+        Object.assign(this.addEvent, editedData);
+        this.editEvent(editedData);
+      },
+      formatDate(event) {
+        if (event.DateTo <= event.DateFrom) {
+          return moment(event.DateFrom).format("DD.MM");
+        } else {
+          return (
+            moment(event.DateFrom).format("DD.MM") +
+            " - " +
+            moment(event.DateTo).format("DD.MM")
+          );
+        }
+      },
+      formatTime(time) {
+        if (time) {
+          return time.slice(0, 5);
+        }
       }
     }
-  }
   )
 };
 </script>
